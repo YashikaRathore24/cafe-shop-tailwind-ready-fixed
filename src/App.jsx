166cfb2b3fa1028409c9f35352f1155be523822f
Yashika Rathore <yashikarathore24@gmail.com>
2026-06-29 11:59:51 +0530

    minior changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 
-import Header from "./components/headers/header";
+import Header from "./components/headers/Header";
 import Home from "./components/home/Home";
 import About from "./components/About/About";
 import Features from "./components/features/Features";
 import Menu from "./components/menu/Menu";
 import Gallery from "./components/gallery/Gallery";
-import Contact from "./components/contact/Contact";
+import Contact from "./components/Contact/Contact";
 import Testinomal from "./components/testominal/Testinomal";
 // import Reservation from "./components/reservation/Reservation";
 import Footer from "./components/footer/Footer";
